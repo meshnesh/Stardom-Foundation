149dd6a461db19030aa4cfbe913e314db190c454
David Kezi <davidkezi6@gmail.com>
2016-05-14 22:50:02 +0300

    put nav bar on all the pages mobile and desktop

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ $(document).foundation()
 $('.parallax-window').parallax({
     imageSrc: '/path/to/image.jpg'
 });
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
